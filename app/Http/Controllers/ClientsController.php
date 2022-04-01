e2c1970514aa1e32dcd70c9f3d30267b6cdedeb1
Abdullayev98 <79930634+Abdullayev98@users.noreply.github.com>
2022-04-01 17:05:20 +0500

    Clients data modified

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -4,11 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Models\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ClientsController extends Controller
 {
     public function clients(){
         $clients = Client::all();
+
+        
+ 
+$clients = DB::table('clients')
+            ->join('products', 'clients.id', '=', 'products.client_id')
+            ->select('clients.name','clients.family','products.title','products.amount')
+            ->get();
+
         $clients_json = json_encode($clients);
         return view('clients', compact('clients_json'));
     }
